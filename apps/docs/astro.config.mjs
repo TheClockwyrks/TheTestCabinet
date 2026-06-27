45f83c30466e8470edb9db0301999761b5979b2f
Zach Wilson <zach.wilson@clockwyrks.com>
2026-06-27 16:47:36 -0700

    docs: add the v0.3.2 changelog

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -167,6 +167,7 @@ export default defineConfig({
           label: "Changelogs",
           collapsed: true,
           items: [
+            "changelogs/v0.3.2",
             "changelogs/v0.3.1",
             "changelogs/v0.3.0",
             "changelogs/v0.2.0",
