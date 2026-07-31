088e8918b245a5c454f3e2622e5c33abbaac9225
Zach Wilson <zach.wilson@clockwyrks.com>
2026-07-31 08:36:17 -0700

    declank: delete the v0.7.0 changelog
    This isn't supposed to be created until the release is ready

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -254,7 +254,6 @@ export default defineConfig({
           label: "Changelogs",
           collapsed: true,
           items: [
-            "changelogs/v0.7.0",
             "changelogs/v0.6.1",
             "changelogs/v0.6.0",
             "changelogs/v0.5.1",
